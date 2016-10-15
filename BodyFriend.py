2ab58795abd8ca69fc8dacf03eaadab5e21a7fe3
mary <maria.chietera@gmail.com>
2016-10-15 18:00:05 +0200

    resize and one_time keyboard

--- BodyFriend.py
+++ BodyFriend.py
@@ -14,21 +14,33 @@ def handle(msg):
 		#bot.sendMessage(chat_id, msg['from']['first_name'])
 		#print(msg['text'])
 		input = str(msg['text']).lower()
-		if input == 'hallo':
+		if input == 'hallo' or input == 'hi':
 			# # give a descriptive hint
 		   bot.sendMessage(chat_id,"Hallo "+ msg['from']['first_name']+"!\nWelcome back!")
 		   bot.sendMessage(chat_id, "What’s up?",
                             reply_markup=ReplyKeyboardMarkup(
                                 keyboard=[
                                     [KeyboardButton(text="Have a question"), KeyboardButton(text="Wanna track symptoms")]
-                                ]
+                                ], 
+								resize_keyboard=True,
+								one_time_keyboard=True
                             ))#,force_reply=True)
 		elif input == 'have a question':
-			bot.sendMessage(chat_id,"test1")			
+			bot.sendMessage(chat_id,"Just tell me!")
+		elif 'period' in input:
+			#time.sleep(0.9)
+			bot.sendMessage(chat_id,"OK. Let’s stay cool for a minute ok? I will check some data.")
+			time.sleep(1.5)
+			bot.sendMessage(chat_id,"Ok, I got something.")
+			time.sleep(1.5)
+			bot.sendMessage(chat_id,"Your average cycle is 28 days. Today you are on day 30. \nDelays of *2-4 days are very usual and normal*.")
+			time.sleep(1.5)
+			bot.sendMessage(chat_id,"In Berlin alone, 67% of women your age. \nhave periods delayed by 3 days at least 5 times a year.")
+	
 		elif input == 'wanna track symptoms':
 			bot.sendMessage(chat_id,"test2")
 		else:
-			bot.sendMessage(chat_id,"Sorry, I don't understand")
+			bot.sendMessage(chat_id,"Sorry, I can't understand")
 
 TOKEN = "288580874:AAEr0kSYI6WeRmOiKzBOrOWr595cG-ZVqMk" #sys.argv[1]  # get token from command-line
 
